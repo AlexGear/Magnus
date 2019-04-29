ac1dda9076d8d2d3f2bdcc47c0d399b2627cbf8e
Alex <alexey-bycov@yandex.ru>
2019-04-29 21:47:57 +0700

    Add database setup script

---
+++ MagnusServer/db_setup/db_setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+CONTAINER=magnus-db
+USER=admin
+PASSWORD=ausrotten
+sudo docker exec -i $CONTAINER mysql -u $USER --password=$PASSWORD -e "CREATE DATABASE IF NOT EXISTS magnus; CREATE DATABASE IF NOT EXISTS magnus_test;"
+cat db.sql | sudo docker exec -i $CONTAINER mysql -u $USER --password=$PASSWORD magnus
+cat db.sql | sudo docker exec -i $CONTAINER mysql -u $USER --password=$PASSWORD magnus_test
