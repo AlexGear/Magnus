6728cac6d5057d84491d7c65f82833391b21b35c
Alex <alexey-bycov@yandex.ru>
2019-04-29 23:01:24 +0700

    Implement app exit by typing 'exit'

--- MagnusServer/src/main/java/ru/eltex/magnus/server/App.java
+++ MagnusServer/src/main/java/ru/eltex/magnus/server/App.java
@@ -1,7 +1,21 @@
 package ru.eltex.magnus.server;
 
-public class App {
-    public static void main(String[] args) {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) throws IOException {
+        ApplicationContext context = SpringApplication.run(App.class, args);
+        System.setProperty("java.awt.headless", "false");
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(System.in))) {
+            while (!"exit".equals(in.readLine().trim().toLowerCase())) ;
+        }
+        SpringApplication.exit(context);
     }
 }
