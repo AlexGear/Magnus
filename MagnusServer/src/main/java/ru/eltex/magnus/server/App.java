f82e7c73be9d6999a19f4798cc90c4bc789767d3
Alex <alexey-bycov@yandex.ru>
2019-05-03 21:55:40 +0700

    Finish Database selects

--- MagnusServer/src/main/java/ru/eltex/magnus/server/App.java
+++ MagnusServer/src/main/java/ru/eltex/magnus/server/App.java
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import ru.eltex.magnus.server.db.StoragesProvider;
-import ru.eltex.magnus.server.db.dataclasses.Admin;
-import ru.eltex.magnus.server.db.dataclasses.Department;
-import ru.eltex.magnus.server.db.dataclasses.Employee;
+import ru.eltex.magnus.server.db.dataclasses.*;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -15,24 +13,6 @@ import java.io.InputStreamReader;
 @SpringBootApplication
 public class App {
     public static void main(String[] args) throws IOException {
-        for(Employee employee : StoragesProvider.getEmployeesStorage().getAllEmployees()) {
-            System.out.println(employee.getLogin());
-            System.out.println(employee.getPassword());
-            System.out.println(employee.getName());
-            System.out.println(employee.getDepartment().getId());
-            System.out.println(employee.getDepartment().getName());
-            System.out.println(employee.getJobName());
-            System.out.println(employee.getPhoneNumber());
-            System.out.println(employee.getEmail());
-        }
-        for(Department department : StoragesProvider.getDepartmentsStorage().getAllDepartments()) {
-            System.out.println(department.getId());
-            System.out.println(department.getName());
-        }
-        Department department = StoragesProvider.getDepartmentsStorage().getDepartmentById(1);
-        System.out.println(department.getId());
-        System.out.println(department.getName());
-
         ApplicationContext context = SpringApplication.run(App.class, args);
         System.setProperty("java.awt.headless", "false");
         StreamersServer.start();
--- MagnusServer/src/main/java/ru/eltex/magnus/server/db/Database.kt
+++ MagnusServer/src/main/java/ru/eltex/magnus/server/db/Database.kt
@@ -2,11 +2,8 @@ package ru.eltex.magnus.server.db
 
 import ru.eltex.magnus.server.db.dataclasses.*
 import ru.eltex.magnus.server.db.storages.*
-import java.lang.UnsupportedOperationException
 
 import java.sql.*
-import java.util.ArrayList
-import java.util.Collections
 
 internal class Database(properties: DatabaseProperties) : EmployeesStorage, DepartmentsStorage,
         OfflineStreamersStorage, ViewersStorage, AdminStorage {
@@ -19,15 +16,15 @@ internal class Database(properties: DatabaseProperties) : EmployeesStorage, Depa
         private const val ADMIN_TABLE = "admin"
     }
 
-    private val connectionURL: String = properties.connectionURL
-    private val login: String = properties.login
-    private val password: String = properties.password
+    private val connectionURL = properties.connectionURL
+    private val databaseLogin = properties.login
+    private val databasePassword = properties.password
 
     override fun getAdmin(): Admin? {
         val sql = "SELECT login, password FROM $ADMIN_TABLE"
         return try {
-            executeQuery(sql) { rs ->
-                if (rs.first()) Admin(rs["login"], rs["password"]) else null
+            selectFirstOrNull(sql) { rs ->
+                Admin(rs["login"], rs["password"])
             }
         } catch (e: SQLException) {
             e.printStackTrace()
@@ -38,8 +35,8 @@ internal class Database(properties: DatabaseProperties) : EmployeesStorage, Depa
     override fun getDepartmentById(id: Int): Department? {
         val sql = "SELECT name FROM $DEPARTMENTS_TABLE WHERE id = ?"
         return try {
-            executeQueryArgs(sql, id) { rs ->
-                if (rs.first()) Department(id, rs["name"]) else null
+            selectFirstOrNull(sql, id) { rs ->
+                Department(id, rs["name"])
             }
         } catch (e: SQLException) {
             e.printStackTrace()
@@ -50,12 +47,8 @@ internal class Database(properties: DatabaseProperties) : EmployeesStorage, Depa
     override fun getAllDepartments(): List<Department>? {
         val sql = "SELECT id, name FROM $DEPARTMENTS_TABLE"
         return try {
-            executeQuery(sql) { rs ->
-                val result = mutableListOf<Department>()
-                while (rs.next()) {
-                    result += Department(rs["id"], rs["name"])
-                }
-                result
+            selectMany(sql) { rs ->
+                Department(rs["id"], rs["name"])
             }
         } catch (e: SQLException) {
             e.printStackTrace()
@@ -72,11 +65,8 @@ internal class Database(properties: DatabaseProperties) : EmployeesStorage, Depa
                 " ON $DEPARTMENTS_TABLE.id = department_id" +
                 " WHERE login = ?"
         return try {
-            executeQueryArgs(sql, login) { rs ->
-                if (!rs.first()) {
-                    null
-                }
-                else with(Employee()) {
+            selectFirstOrNull(sql, login) { rs ->
+                with(Employee()) {
                     this.login = login
                     password = rs["password"]
                     name = rs["$EMPLOYEES_TABLE.name"]
@@ -101,21 +91,17 @@ internal class Database(properties: DatabaseProperties) : EmployeesStorage, Depa
                 " LEFT JOIN $DEPARTMENTS_TABLE" +
                 " ON $DEPARTMENTS_TABLE.id = department_id"
         return try {
-            executeQuery(sql) { rs ->
-                val result = mutableListOf<Employee>()
-                while(rs.next()) {
-                    result += with(Employee()) {
-                        login = rs["login"]
-                        password = rs["password"]
-                        name = rs["$EMPLOYEES_TABLE.name"]
-                        department = Department(rs["department_id"], rs["$DEPARTMENTS_TABLE.name"])
-                        jobName = rs["job_name"]
-                        phoneNumber = rs["phone_number"]
-                        email = rs["email"]
-                        this
-                    }
+            selectMany(sql) { rs ->
+                with(Employee()) {
+                    login = rs["login"]
+                    password = rs["password"]
+                    name = rs["$EMPLOYEES_TABLE.name"]
+                    department = Department(rs["department_id"], rs["$DEPARTMENTS_TABLE.name"])
+                    jobName = rs["job_name"]
+                    phoneNumber = rs["phone_number"]
+                    email = rs["email"]
+                    this
                 }
-                result
             }
         } catch (e: SQLException) {
             e.printStackTrace()
@@ -124,48 +110,99 @@ internal class Database(properties: DatabaseProperties) : EmployeesStorage, Depa
     }
 
     override fun getOfflineStreamerByLogin(login: String): OfflineStreamer? {
-        return null
+        val sql = "SELECT last_seen FROM $OFFLINE_STREAMERS_TABLE WHERE login = ?"
+        return try {
+            selectFirstOrNull(sql, login) { rs ->
+                OfflineStreamer(login, rs["last_seen"])
+            }
+        }
+        catch (e: SQLException) {
+            e.printStackTrace()
+            null
+        }
     }
 
     override fun getAllOfflineStreamers(): List<OfflineStreamer>? {
-        return null
+        val sql = "SELECT login, last_seen FROM $OFFLINE_STREAMERS_TABLE"
+        return try {
+            executeQuery(sql) { rs ->
+                val result = mutableListOf<OfflineStreamer>()
+                while(rs.next()) {
+                    result += OfflineStreamer(rs["login"], rs["last_seen"])
+                }
+                result
+            }
+        }
+        catch (e: SQLException) {
+            e.printStackTrace()
+            null
+        }
     }
 
     override fun getViewerByLogin(login: String): Viewer? {
-        return null
+        val sql = "SELECT password, name FROM $VIEWERS_TABLE WHERE login = ?"
+        return try {
+            selectFirstOrNull(sql, login) { rs ->
+                Viewer(login, rs["password"], rs["name"])
+            }
+        }
+        catch (e: SQLException) {
+            e.printStackTrace()
+            null
+        }
     }
 
     override fun getAllViewers(): List<Viewer>? {
-        return null
+        val sql = "SELECT login, password, name FROM $VIEWERS_TABLE WHERE login"
+        return try {
+            selectMany(sql) { rs ->
+                Viewer(rs["login"], rs["password"], rs["name"])
+            }
+        }
+        catch (e: SQLException) {
+            e.printStackTrace()
+            null
+        }
     }
 
-    @Throws(SQLException::class)
     private fun openConnection(): Connection {
-        return DriverManager.getConnection(connectionURL, login, password)
+        return DriverManager.getConnection(connectionURL, databaseLogin, databasePassword)
     }
 
-    @Throws(SQLException::class)
-    private fun <T> executeQuery(sql: String, action: (ResultSet) -> T) : T {
+    private fun <T> executeQuery(sql: String, vararg args: Any, action: (ResultSet) -> T) : T {
         openConnection().use { connection ->
-            connection.createStatement().use { statement ->
+            if(args.isNotEmpty()) {
+                connection.prepareStatement(sql).use { statement ->
+                    for (i in args.indices) {
+                        statement.setObject(i + 1, args[i])
+                    }
+                    return statement.executeQuery().use(action)
+                }
+            }
+            else connection.createStatement().use { statement ->
                 return statement.executeQuery(sql).use(action)
             }
         }
     }
 
-    @Throws(SQLException::class)
-    private fun <T> executeQueryArgs(sql: String, vararg args: Any, action: (ResultSet) -> T) : T {
-        openConnection().use { connection ->
-            connection.prepareStatement(sql).use { statement ->
-                for (i in args.indices) {
-                    statement.setObject(i + 1, args[i])
-                }
-                return statement.executeQuery().use(action)
-            }
+    private fun <T> selectFirstOrNull(sql: String, vararg args: Any, selector: (ResultSet) -> T) : T? {
+        return executeQuery(sql, *args) { rs ->
+            if (rs.first())
+                selector(rs)
+            else
+                null
         }
     }
 
-    private inline operator fun <reified T> ResultSet.get(s: String) : T {
-        return this.getObject(s) as T
+    private fun <T> selectMany(sql: String, selector: (ResultSet) -> T) : List<T> {
+        return executeQuery(sql) { rs ->
+            val result = mutableListOf<T>()
+            while (rs.next()) {
+                result += selector(rs)
+            }
+            result
+        }
     }
+    
+    private inline operator fun <reified T> ResultSet.get(s: String) = this.getObject(s) as T
 }
