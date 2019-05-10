8553117023d2c7c55ae0abb2b17895d95592e614
Alex <alexey-bycov@yandex.ru>
2019-05-10 21:04:12 +0700

    Implement new properties file creation on loading failure

--- MagnusStreamer/src/main/java/ru/eltex/magnus/streamer/App.java
+++ MagnusStreamer/src/main/java/ru/eltex/magnus/streamer/App.java
@@ -11,10 +11,18 @@ public class App {
 
     public static void main(String[] args) {
         if (!PROPERTIES.load()) {
-            String message = "Cannot load properties file: '" + PROPERTIES_FILE_PATH + "'";
-            String title = "Error loading properties";
-            JOptionPane.showMessageDialog(null, message, title, JOptionPane.ERROR_MESSAGE);
-            System.exit(1);
+            System.out.println("Creating new properties file...");
+            PROPERTIES.setServerAddress("");
+            PROPERTIES.setServerPort(0);
+            PROPERTIES.setLogin("");
+            PROPERTIES.setPassword("");
+            if(!PROPERTIES.save()) {
+                String message = "Could not load properties file '" + PROPERTIES_FILE_PATH +
+                        "'.\nTried to recreate the file but also failed";
+                String title = "Error loading properties";
+                JOptionPane.showMessageDialog(null, message, title, JOptionPane.ERROR_MESSAGE);
+                System.exit(1);
+            }
         }
         GUI.init();
         STREAMER.init();
