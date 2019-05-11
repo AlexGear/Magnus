ce15f5ea2ad2fab9e1aed555185b95fe2baee299
Alex <alexey-bycov@yandex.ru>
2019-05-11 16:08:46 +0700

    Refactor security configuration class a bit

--- MagnusServer/src/main/java/ru/eltex/magnus/server/ViewersSecurityConfiguration.java
+++ MagnusServer/src/main/java/ru/eltex/magnus/server/ViewersSecurityConfiguration.java
@@ -3,6 +3,7 @@ package ru.eltex.magnus.server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,7 +15,7 @@ import ru.eltex.magnus.server.db.dataclasses.Viewer;
 
 @Configuration
 @EnableWebSecurity
-public class EmployeeSecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class ViewersSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -33,15 +34,16 @@ public class EmployeeSecurityConfiguration extends WebSecurityConfigurerAdapter 
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder authenticationMgr) throws Exception {
+        InMemoryUserDetailsManagerConfigurer<AuthenticationManagerBuilder> builder =
+                authenticationMgr.inMemoryAuthentication();
+
         Admin admin = StoragesProvider.getAdminStorage().getAdmin();
-        authenticationMgr.inMemoryAuthentication()
-                .withUser(admin.getLogin()).password(admin.getPassword()).roles("ADMIN")
-                .and().passwordEncoder(new BCryptPasswordEncoder());
+        builder.withUser(admin.getLogin()).password(admin.getPassword()).roles("ADMIN");
 
         for (Viewer viewer : StoragesProvider.getViewersStorage().getAllViewers()) {
-            authenticationMgr.inMemoryAuthentication()
-                    .withUser(viewer.getLogin()).password(viewer.getPassword()).roles("VIEWER")
-                    .and().passwordEncoder(new BCryptPasswordEncoder());
+            builder.withUser(viewer.getLogin()).password(viewer.getPassword()).roles("VIEWER");
         }
+
+        builder.passwordEncoder(new BCryptPasswordEncoder());
     }
 }
