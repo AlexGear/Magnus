d7e62f4f75172b8d9652b4079a58b7f73c5672f6
Alex <alexey-bycov@yandex.ru>
2019-05-13 23:58:42 +0700

    Implement employees insertion on admin's page

--- MagnusServer/src/main/java/ru/eltex/magnus/server/controllers/AdminPageController.java
+++ MagnusServer/src/main/java/ru/eltex/magnus/server/controllers/AdminPageController.java
@@ -3,8 +3,10 @@ package ru.eltex.magnus.server.controllers;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import ru.eltex.magnus.server.db.StoragesProvider;
 import ru.eltex.magnus.server.db.dataclasses.Department;
 import ru.eltex.magnus.server.db.dataclasses.Employee;
@@ -19,6 +21,43 @@ public class AdminPageController {
         List<Employee> employees = StoragesProvider.getEmployeesStorage().getAllEmployees();
         employees.forEach(e -> e.setPassword(""));
         return employees;
+    }
+
+    @RequestMapping("/admin/add_employee")
+    public ResponseEntity<String> addEmployee(
+            @RequestParam("login") String login,
+            @RequestParam("password") String password,
+            @RequestParam("name") String name,
+            @RequestParam("departmentId") int departmentId,
+            @RequestParam("jobName") String jobName,
+            @RequestParam("phoneNumber") String phoneNumber,
+            @RequestParam("email") String email
+    ) {
+        if (login.isEmpty())        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("login is empty");
+        if (password.isEmpty())     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("password is empty");
+        if (name.isEmpty())         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("name is empty");
+        if (jobName.isEmpty())      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("jobName is empty");
+        if (phoneNumber.isEmpty())  return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("phoneNumber is empty");
+        if (email.isEmpty())        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("email is empty");
+
+        Employee e = new Employee();
+        e.setLogin(login);
+        e.setPassword(new BCryptPasswordEncoder().encode(password));
+        e.setName(name);
+        Department d = StoragesProvider.getDepartmentsStorage().getDepartmentById(departmentId);
+        if (d == null) {
+            String body = "Department with id '" + departmentId + "' not found";
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(body);
+        }
+        e.setDepartment(d);
+        e.setJobName(jobName);
+        e.setPhoneNumber(phoneNumber);
+        e.setEmail(email);
+
+        if(StoragesProvider.getEmployeesStorage().insertEmployee(e)) {
+            return ResponseEntity.ok("OK");
+        }
+        return ResponseEntity.status(520).body("Unknown error");
     }
 
     @RequestMapping("/admin/edit_employee")
@@ -57,7 +96,7 @@ public class AdminPageController {
     }
 
     @RequestMapping("/admin/change_employee_password")
-    public ResponseEntity changeEmployeePassword(
+    public ResponseEntity<String> changeEmployeePassword(
             @RequestParam("login") String login,
             @RequestParam("password") String password
     ) {
@@ -75,7 +114,7 @@ public class AdminPageController {
     }
 
     @RequestMapping("/admin/remove_employee")
-    public ResponseEntity removeEmployee(@RequestParam("login") String login) {
+    public ResponseEntity<String> removeEmployee(@RequestParam("login") String login) {
         EmployeesStorage storage = StoragesProvider.getEmployeesStorage();
         if (storage.removeEmployeeByLogin(login)) {
             return ResponseEntity.ok("OK");
